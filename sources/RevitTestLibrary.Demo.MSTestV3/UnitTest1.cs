f3d7f2ef413bdf2b586224cc2ad2a45c9f6ff6fa
NeVeSpl <nevespl@gmail.com>
2023-03-02 09:46:01 +0100

    add more examples

--- sources/RevitTestLibrary.Demo.MSTestV3/UnitTest1.cs
+++ sources/RevitTestLibrary.Demo.MSTestV3/UnitTest1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RevitTestLibrary.MSTest;
@@ -13,7 +16,6 @@ namespace RevitTestLibrary.Demo.MSTestV3
         {
             Console.WriteLine("This is a standard test");
         }
-
         [RevitTestMethod]
         public void Revit_TestMethod_WriteLine(UIApplication uia)
         {
@@ -26,11 +28,58 @@ namespace RevitTestLibrary.Demo.MSTestV3
         {
             Assert.Fail("This is a standard test");
         }
-
         [RevitTestMethod]
         public void Revit_TestMethod_Fail(UIApplication uia)
         {
             Assert.Fail("This code is running inside Revit.");
+        }
+
+
+        [RevitTestMethod]
+        public void Revit_CreateNewProjectDocument(UIApplication uia)
+        {
+            var document = uia.Application.NewProjectDocument(UnitSystem.Metric);
+            Console.WriteLine(document.Title);
+            document.Close(false);
+        }
+
+
+        [RevitTestMethod]
+        public void Revit_OpenProjectDocument(UIApplication uia)
+        {
+            var path = Path.Combine(GetTestDirectory(), @"..\..\assets\rst_basic_sample_project.rvt");
+            var document = uia.Application.OpenDocumentFile(path);
+
+            var collector = new FilteredElementCollector(document).WhereElementIsNotElementType().OfCategory(BuiltInCategory.OST_Walls);
+            var count = collector.GetElementCount();
+
+            Assert.AreEqual(9, count);
+            document.Close(false);
+        }
+
+
+        [RevitTestMethod]
+        [DataRow(BuiltInCategory.OST_Walls, 9)]
+        [DataRow(BuiltInCategory.OST_Columns, 0)]
+        public void Revit_DataDrivenTest(UIApplication uia, BuiltInCategory inputCategory, int expectedCount)
+        {
+            var path = Path.Combine(GetTestDirectory(), @"..\..\assets\rst_basic_sample_project.rvt");
+            var document = uia.Application.OpenDocumentFile(path);
+
+            var collector = new FilteredElementCollector(document).WhereElementIsNotElementType().OfCategory(inputCategory);
+            var count = collector.GetElementCount();
+
+            Assert.AreEqual(expectedCount, count);
+            document.Close(false);
+        }
+
+
+        private static string GetTestDirectory()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
         }
     }
 }
